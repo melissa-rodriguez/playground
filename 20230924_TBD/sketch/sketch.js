377eca0aef5f4d2291c1b46c8e2e454c782c1482
Melissa Rodriguez <hello@melissa-rodriguez.com>
2023-09-26 20:15:17 -0400

    create grid with custom shape class

--- 20230924_TBD/sketch/sketch.js
+++ 20230924_TBD/sketch/sketch.js
@@ -1,32 +1,57 @@
-let shape;
-
+let shapes = [];
+// let shape;
 function setup() {
   createCanvas(1000, 1000);
   // randomSeed(1); //horiz
   // randomSeed(3); //back diag
   // randomSeed(6); //vertical
   // randomSeed(39); //forward diag
-  shape = new CornerSquare();
+  // shape = new CornerSquare(width/2, height/2, 100, 100);
+
+  let cols = 4;
+  let rows = 4;
+  let spacing = 150;
+
+  let boxW = (width - cols * spacing) / cols;
+  let boxH = (height - rows * spacing) / rows;
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let xpos = (boxW + spacing) / 2 + (boxW + spacing) * i;
+      let ypos = (boxH + spacing) / 2 + (boxH + spacing) * j;
+      // rect(xpos, ypos, boxW, boxH);
+      let shape = new CornerSquare(xpos, ypos, boxW, boxH);
+      shapes.push(shape);
+    }
+  }
+
+
 
 }
 
 function draw() {
   background(240);
+  for (let i = 0; i < shapes.length; i++) {
+    let shape = shapes[i];
+    shape.show();
+  }
 
-  shape.show();
+  // shape.show();
+
+
 
   noLoop();
 }
 
 class CornerSquare {
-  constructor() {
-    this.pos = createVector(width / 2, height / 2);
-    this.w = 200;
-    this.h = 200;
+  constructor(x, y, w_, h_) {
+    // this.pos = createVector(width / 2, height / 2);
+    this.pos = createVector(x, y);
+    this.w = w_;
+    this.h = h_;
     this.r = this.w / 3.5; //ellipse size
 
     let ellipseTypes = ["horizontal", "vertical"];
-    this.ellipseType = ellipseTypes[0];
+    this.ellipseType = ellipseTypes[round(random(1))];
 
     //clockwise
     this.p1 = new squarePoint(this.pos.x - this.w / 2, this.pos.y - this.h / 2); //upper left pos
@@ -64,7 +89,6 @@ class CornerSquare {
     } else {
       this.connectionType = "horizontal";
     }
-    console.log(this.connectionType);
   }
 
   pickFromList(list) {
@@ -73,30 +97,85 @@ class CornerSquare {
   }
 
   show() {
-    strokeWeight(10);
-    stroke('red');
-    noFill();
-    let holeWidth = this.r * 2.5;
-    let holeHeight = this.r;
+    strokeWeight(20);
+    strokeCap(SQUARE);
+    // stroke('red');
+    // noFill();
 
-    if(this.ellipseType == "horizontal"){
-      this.horizontalEllipseRenders(holeWidth, holeHeight); 
+
+    if (this.ellipseType == "horizontal") {
+      let holeWidth = this.r * 2.5;
+      let holeHeight = this.r;
+      this.horizontalRender(holeWidth, holeHeight);
+    } else if (this.ellipseType == "vertical") {
+      let holeWidth = this.r;
+      let holeHeight = this.r * 2.5;
+      if (this.connectionType != "diagonal_forward" && this.connectionType != "diagonal_backward") {
+        this.verticalRender(holeWidth, holeHeight);
+      }
     }
 
 
   }
 
-  horizontalEllipseRenders(holeWidth, holeHeight) {
+  verticalRender(holeWidth, holeHeight) {
     //----ELLIPSE----
+    noStroke();
+    fill(0);
     for (let i = 0; i < this.corners.length; i++) {
       let pt = this.corners[i];
       if (pt.filled) {
-        point(pt.pos.x, pt.pos.y);
+        ellipse(pt.pos.x, pt.pos.y, holeWidth, holeHeight);
+      }
+    }
+
+    //-----CONNECTING LINES-----
+    stroke('red');
+    noFill();
+    let arcDiameter = this.w;
+    let chooseRandom = random(1);
+
+    if (this.connectionType == "vertical") {
+      if (this.p1.filled || this.p4.filled) {
+        if (chooseRandom > 0.5) {
+          //right
+          arc(this.p1.pos.x - holeWidth/2, (this.p1.pos.y + this.p4.pos.y) / 2, arcDiameter, arcDiameter, PI + PI / 2, TAU + PI / 2);
+        } else {
+          //left
+          arc(this.p1.pos.x + holeWidth/2, (this.p1.pos.y + this.p4.pos.y) / 2, arcDiameter, arcDiameter, PI / 2, PI + PI / 2);
+        }
+      } else {
+        if (chooseRandom > 0.5) {
+          //right
+          arc(this.p2.pos.x - holeWidth/2, (this.p2.pos.y + this.p3.pos.y) / 2, arcDiameter, arcDiameter, PI + PI / 2, TAU + PI / 2);
+        } else {
+          //left
+          arc(this.p2.pos.x + holeWidth/2, (this.p2.pos.y + this.p3.pos.y) / 2, arcDiameter, arcDiameter, PI / 2, PI + PI / 2);
+        }
+      }
+    } else if (this.connectionType == "horizontal") {
+      if (this.p1.filled || this.p2.filled) {
+        line(this.p1.pos.x - holeWidth/2, this.p1.pos.y, this.p2.pos.x + holeWidth/2, this.p2.pos.y);
+      } else {
+        line(this.p4.pos.x - holeWidth/2, this.p4.pos.y, this.p3.pos.x + holeWidth/2, this.p3.pos.y);
+      }
+    }
+  }
+
+  horizontalRender(holeWidth, holeHeight) {
+    //----ELLIPSE----
+    noStroke();
+    fill(0);
+    for (let i = 0; i < this.corners.length; i++) {
+      let pt = this.corners[i];
+      if (pt.filled) {
         ellipse(pt.pos.x, pt.pos.y, holeWidth, holeHeight);
       }
     }
 
     //----CONNECTING LINES-----
+    stroke('red');
+    noFill();
     let chooseRandom = random(1);
     let arcDiameter = this.w;
 
@@ -105,12 +184,12 @@ class CornerSquare {
 
       if (chooseRandom > 0.5) {
         //go over
-        line(this.p3.pos.x, this.p3.pos.y, this.p3.pos.x, this.p1.pos.y);
-        arc(this.p3.pos.x - arcDiameter / 2, this.p1.pos.y, arcDiameter, arcDiameter, PI, 0);
+        line(this.p3.pos.x, this.p3.pos.y + holeHeight/2, this.p3.pos.x, this.p1.pos.y + holeHeight/2);
+        arc(this.p3.pos.x - arcDiameter / 2, this.p1.pos.y + holeHeight/2, arcDiameter, arcDiameter, PI, 0);
       } else {
         //go under
-        line(this.p1.pos.x, this.p1.pos.y, this.p1.pos.x, this.p3.pos.y);
-        arc(this.p1.pos.x + arcDiameter / 2, this.p3.pos.y, arcDiameter, arcDiameter, 0, PI);
+        line(this.p1.pos.x, this.p1.pos.y - holeHeight/2, this.p1.pos.x, this.p3.pos.y - holeHeight/2);
+        arc(this.p1.pos.x + arcDiameter / 2, this.p3.pos.y - holeHeight/2, arcDiameter, arcDiameter, 0, PI);
 
       }
       //determine ellipsetype
@@ -118,35 +197,35 @@ class CornerSquare {
 
       if (chooseRandom > 0.5) {
         //go over
-        line(this.p4.pos.x, this.p4.pos.y, this.p4.pos.x, this.p2.pos.y);
-        arc(this.p4.pos.x + arcDiameter / 2, this.p2.pos.y, arcDiameter, arcDiameter, PI, TAU);
+        line(this.p4.pos.x, this.p4.pos.y + holeHeight/2, this.p4.pos.x, this.p2.pos.y + holeHeight/2);
+        arc(this.p4.pos.x + arcDiameter / 2, this.p2.pos.y + holeHeight/2, arcDiameter, arcDiameter, PI, TAU);
       } else {
         //go under
-        line(this.p2.pos.x, this.p2.pos.y, this.p2.pos.x, this.p4.pos.y);
-        arc(this.p2.pos.x - arcDiameter / 2, this.p4.pos.y, arcDiameter, arcDiameter, 0, PI);
+        line(this.p2.pos.x, this.p2.pos.y - holeHeight/2, this.p2.pos.x, this.p4.pos.y - holeHeight/2);
+        arc(this.p2.pos.x - arcDiameter / 2, this.p4.pos.y - holeHeight/2, arcDiameter, arcDiameter, 0, PI);
 
       }
     } else if (this.connectionType == "vertical") {
       if (this.p1.filled || this.p4.filled) {
-        line(this.p1.pos.x, this.p1.pos.y, this.p4.pos.x, this.p4.pos.y);
+        line(this.p1.pos.x, this.p1.pos.y - holeHeight/2, this.p4.pos.x, this.p4.pos.y + holeHeight/2);
       } else {
-        line(this.p2.pos.x, this.p2.pos.y, this.p3.pos.x, this.p3.pos.y);
+        line(this.p2.pos.x, this.p2.pos.y - holeHeight/2, this.p3.pos.x, this.p3.pos.y + holeHeight/2);
       }
     } else if (this.connectionType == "horizontal") {
       if (this.p1.filled || this.p2.filled) {
         if (chooseRandom > 0.5) {
 
-          arc((this.p2.pos.x + this.p1.pos.x) / 2, this.p1.pos.y, arcDiameter, arcDiameter, PI, TAU);
+          arc((this.p2.pos.x + this.p1.pos.x) / 2, this.p1.pos.y + holeHeight/2, arcDiameter, arcDiameter, PI, TAU);
         } else {
 
-          arc((this.p2.pos.x + this.p1.pos.x) / 2, this.p1.pos.y, arcDiameter, arcDiameter, 0, PI);
+          arc((this.p2.pos.x + this.p1.pos.x) / 2, this.p1.pos.y - holeHeight/2, arcDiameter, arcDiameter, 0, PI);
         }
       } else {
         if (chooseRandom > 0.5) {
-          arc((this.p4.pos.x + this.p3.pos.x) / 2, this.p4.pos.y, arcDiameter, arcDiameter, PI, TAU);
+          arc((this.p4.pos.x + this.p3.pos.x) / 2, this.p4.pos.y + holeHeight/2, arcDiameter, arcDiameter, PI, TAU);
         } else {
 
-          arc((this.p4.pos.x + this.p3.pos.x) / 2, this.p4.pos.y, arcDiameter, arcDiameter, 0, PI);
+          arc((this.p4.pos.x + this.p3.pos.x) / 2, this.p4.pos.y - holeHeight/2, arcDiameter, arcDiameter, 0, PI);
         }
       }
     }

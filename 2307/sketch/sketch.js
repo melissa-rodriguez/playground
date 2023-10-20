ec05c11a07e5574d7a626a3c3dd8cc56c5eea65b
melissa rodriguez <hello@melissa-rodriguez.com>
2023-10-20 07:53:10 -0400

    experiment with blendmode

--- 2307/sketch/sketch.js
+++ 2307/sketch/sketch.js
@@ -1,10 +1,40 @@
+let circleR = 0;
+let grow = false;
+let col = 0;
+
 function setup() {
   createCanvas(1000, 1000);
+  colorMode(HSB);
+  background(240, 255, 255);
 
 }
 
 function draw() {
-  background(240);
-  ellipse(width / 2, height / 2, 200);
-  
+  blendMode(DIFFERENCE);
+
+  if (circleR > width*2) {
+    grow = false;
+    noLoop();
+    circleR = 0;
+  }
+
+  if (grow) {
+    noStroke();
+    fill(col)
+    ellipse(mouseX, mouseY, circleR * 2, circleR * 2);
+    circleR += 5;
+  }
+}
+
+function mousePressed() {
+  // col = color(random(255), random(255), 100);
+  col = color(80, random(100, 255), 255); 
+  grow = true;
+  loop();
+}
+
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){
+    save("output.png"); 
+  }
 }

467bdb41ae81fbfcec7e390fd9fcca6093378192
melissa rodriguez <hello@melissa-rodriguez.com>
2024-02-18 08:15:51 -0500

    detects if all vehicles have arrived to target, add multiple nodes

--- 2024/24001/sketch/sketch.js
+++ 2024/24001/sketch/sketch.js
@@ -1,30 +1,43 @@
-let node;
+let nodes = [];
 
 function setup() {
   createCanvas(1000, 1000);
-  node = new Node(width / 2, height / 2, 5);
+
+  let cols = ['blue', 'green', 'yellow']
+  for (let i = 0; i < 3; i++) {
+    let strokeCol = cols[i];
+    node = new Node(random(width), random(height), int(random(2, 5)), strokeCol);
+    nodes.push(node);
+  }
 }
 
 function draw() {
   background(0);
 
-  node.applyBehaviors();
-  node.show();
+  for (let i = 0; i < nodes.length; i++) {
+    let node = nodes[i];
+    node.applyBehaviors();
+    node.show();
+  }
 
 }
 
 class Node {
-  constructor(x, y, n) {
+  constructor(x, y, n, col) {
     this.pos = createVector(x, y);
     this.numberOfBranches = n;
+    this.strokeCol = col; 
 
     this.vehicles = [];
 
     for (let i = 0; i < this.numberOfBranches; i++) {
-      let vehicle = new Vehicle(this.pos.x, this.pos.y);
+      let wobblyFactor = int(random(0, 20));
+      let vehicle = new Vehicle(this.pos.x, this.pos.y, wobblyFactor);
       this.vehicles.push(vehicle);
       this.target = createVector(random(width), random(height));
     }
+
+    this.allVehiclesArrived = false;
 
 
   }
@@ -42,6 +55,7 @@ class Node {
         vehicle.applyForce(wanderForce.mult(5));
       }
 
+
       let steering = vehicle.arrive(this.target);
       vehicle.applyForce(steering);
     }
@@ -50,31 +64,48 @@ class Node {
   }
 
   show() {
+    stroke(this.strokeCol);
+    let allArrived = true; // Assume all vehicles have arrived initially
     for (let i = 0; i < this.vehicles.length; i++) {
       let vehicle = this.vehicles[i];
       vehicle.update();
       vehicle.show();
+
+      // Check if the current vehicle has not arrived
+      if (!vehicle.hasArrived) {
+        allArrived = false; // Set to false if any vehicle has not arrived
+      }
     }
+
+    // Set allVehiclesArrived to true only if all vehicles have arrived
+    this.allVehiclesArrived = allArrived;
+
+    // if (this.allVehiclesArrived) {
+    //   ellipse(width / 2, height / 2, 200);
+    // }
   }
+
 
 }
 
 
 class Vehicle {
-  constructor(x, y) {
+  constructor(x, y, wobble) {
     this.pos = createVector(x, y);
     this.vel = p5.Vector.random2D();
     this.acc = createVector(0, 0);
     this.maxSpeed = 5;
     this.maxForce = 0.2;
     this.r = 16;
-    this.wobbleFactor = 5; //0 = straight, 3 = shaky, 10 = curvy
+    this.wobbleFactor = wobble; //0 = straight, 3 = shaky, 10 = curvy
 
     this.wanderTheta = 0;
     this.distance = 0;
 
     this.currentPath = [];
     this.paths = [this.currentPath];
+
+    this.hasArrived = false;
   }
 
 
@@ -116,6 +147,11 @@ class Vehicle {
         desiredSpeed = map(this.distance, 0, slowRadius, 0, this.maxSpeed);
       }
     }
+
+    if (frameCount > 5 && this.vel.mag() < .1) {
+      this.hasArrived = true; //has the vehicle arrived to the target?
+    }
+
     force.setMag(desiredSpeed);
     force.sub(this.vel);
     force.limit(this.maxForce);
@@ -189,15 +225,18 @@ class Vehicle {
 
   }
 
-  show() {
-    stroke(255);
+  show(debug = false) {
+    // stroke(255);
     strokeWeight(2);
-    fill(255);
-    push();
-    translate(this.pos.x, this.pos.y);
-    rotate(this.vel.heading());
-    triangle(-this.r, -this.r / 2, -this.r, this.r / 2, this.r, 0);
-    pop();
+
+    if (debug) {
+      fill(255);
+      push();
+      translate(this.pos.x, this.pos.y);
+      rotate(this.vel.heading());
+      triangle(-this.r, -this.r / 2, -this.r, this.r / 2, this.r, 0);
+      pop();
+    }
 
     for (let path of this.paths) {
       beginShape();
@@ -233,20 +272,26 @@ class Vehicle {
   }
 }
 
-class Target extends Vehicle {
-  constructor(x, y) {
-    super(x, y);
-    this.vel = p5.Vector.random2D();
-    this.vel.mult(5);
-  }
 
-  show() {
-    stroke(255);
-    strokeWeight(2);
-    fill("#F063A4");
-    push();
-    translate(this.pos.x, this.pos.y);
-    circle(0, 0, this.r * 2);
-    pop();
+function intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+  // Calculate the direction of the lines
+  let uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) /
+    ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+  let uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) /
+    ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+
+  // If uA and uB are between 0-1, lines are intersecting
+  if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
+    // Intersection point
+    let intersectionX = x1 + (uA * (x2 - x1));
+    let intersectionY = y1 + (uA * (y2 - y1));
+    return {
+      intersects: true,
+      x: intersectionX,
+      y: intersectionY
+    };
   }
+  return {
+    intersects: false
+  };
 }

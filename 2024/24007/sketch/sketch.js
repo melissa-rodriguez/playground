89abbab5d8a89bd4f0a716405eb8ddbb87c6a645
melissa rodriguez <hello@melissa-rodriguez.com>
2024-05-15 20:26:35 -0400

    cmyk colors

--- 2024/24007/sketch/sketch.js
+++ 2024/24007/sketch/sketch.js
@@ -1,10 +1,31 @@
 function setup() {
   createCanvas(1000, 1000);
-
 }
 
+let a = 0; 
 function draw() {
+  blendMode(BLEND);
   background(240);
-  ellipse(width / 2, height / 2, 200);
+
+  blendMode(MULTIPLY);
+
+  noStroke();
+  let r = 100;
   
+  // Magenta circle
+  fill(255, 0, 255);
+  // ellipse(width / 2 - r / 2 * cos(a), height / 2, r * 2);
+  ellipse(width / 2 - r / 2 , height / 2, r * 2);
+
+  // Yellow circle
+  fill(255, 255, 0);
+  // ellipse(width / 2 + r / 2 * cos(a), height / 2, r * 2);
+  ellipse(width / 2 + r / 2 , height / 2, r * 2);
+
+  // Cyan circle
+  fill(0, 255, 255);
+  // ellipse(width / 2, height / 2 - r * sin(a), r * 2);
+  ellipse(width / 2, height / 2 - r , r * 2);
+
+  // a += radians(1); 
 }

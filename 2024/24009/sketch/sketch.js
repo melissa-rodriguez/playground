0f40d26568401c0cfc2f734c8b2216b90839dc33
melissa rodriguez <hello@melissa-rodriguez.com>
2024-05-16 20:38:43 -0400

    noise brush

--- 2024/24009/sketch/sketch.js
+++ 2024/24009/sketch/sketch.js
@@ -1,10 +1,50 @@
+let points = [];
+
 function setup() {
   createCanvas(1000, 1000);
+
+
 
 }
 
 function draw() {
   background(240);
-  ellipse(width / 2, height / 2, 200);
-  
+  // let col = color(0, 150, 255, 45);
+  let col = color(0, 0, 255, 15);
+  // let col = color(242, 92, 5, 40);
+  noStroke();
+  let radius = 400; 
+
+  fill(red(col), green(col), blue(col), 7);
+  // ellipse(width / 2, height / 2, radius, radius + 300);
+
+
+  let noiseMax = .5;
+  let rMax =radius - 100;
+  let rMin = -70;
+  let amt = 100;
+  for (let i = 0; i < amt; i++) {
+    beginShape();
+
+    for (let a = 0; a <= TAU*2; a += radians(4)) {
+      let xoff = map(cos(a), -1, 1, 0, noiseMax);
+      let yoff = map(sin(a), -1, 1, 0, noiseMax);
+      // let r = width / 2;
+      let n = noise(xoff + i * 12345, yoff + i * 12345);
+      let r = map(n, 0, 1, rMin, rMax);
+      let transX = map(noise(xoff + a * random(12345), yoff + a * random(12345)), 0, 1, -100, 100);
+      let transY = map(noise(xoff + a * random(12345), yoff + a * random(12345)), 0, 1, -100, 100);
+      let x = (width / 2) + transX + r * cos(a);
+      let y = height / 2 + transY + r*2 * sin(a);
+      noFill();
+      stroke(col);
+      curveVertex(x, y)
+    }
+    endShape();
+
+  }
+
+
+  noLoop();
+
 }

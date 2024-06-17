5ad0e027e43bdf68406005e35b536f3da8f963e8
melissa rodriguez <hello@melissa-rodriguez.com>
2024-06-17 16:37:18 -0400

    create arc cutout shape

--- 2024/24018/sketch/sketch.js
+++ 2024/24018/sketch/sketch.js
@@ -1,10 +1,80 @@
+
 function setup() {
   createCanvas(1000, 1000);
-
 }
 
 function draw() {
   background(240);
-  ellipse(width / 2, height / 2, 200);
-  
+  //origin(x,y), bottom corner
+  let x = width / 2;
+  let y = height / 2;
+  //width and height of arc
+  let w = 400;
+  let h = 400;
+  //rotate about corner
+  let rotateAngle = 0;
+
+  stroke(0);
+  fill(0);
+  strokeWeight(4);
+  cutArc1(x, y, w, h, rotateAngle);
+
+
+}
+
+function cutArc1(x, y, w, h, rotateAngle) {
+  push();
+  translate(x, y);
+  rotate(rotateAngle);
+  let numPoints = 50;
+  let arc1 = new LerpArc(w / 2, -h / 2, w, h, PI / 2, PI, numPoints);
+  let line1 = new LerpLine(0, -h / 2, 0, 0, numPoints / 2);
+
+
+  beginShape();
+  line1.show();
+  arc1.show();
+  endShape(CLOSE);
+  pop();
+}
+
+class LerpArc {
+  constructor(x_, y_, w_, h_, startAngle_, endAngle_, numPoints_) {
+    this.x = x_;
+    this.y = y_;
+    this.w = w_ / 2;
+    this.h = h_ / 2;
+    this.startAngle = startAngle_;
+    this.endAngle = endAngle_;
+    this.numPoints = numPoints_;
+  }
+
+  show() {
+    for (let i = 0; i <= this.numPoints; i++) {
+      let theta = map(i, 0, this.numPoints, this.startAngle, this.endAngle);
+      let xpos = this.x + this.w * cos(theta);
+      let ypos = this.y + this.h * sin(theta);
+      vertex(xpos, ypos);
+    }
+
+
+  }
+}
+
+class LerpLine {
+  constructor(x1_, y1_, x2_, y2_, amtPoints_) {
+    this.x1 = x1_;
+    this.y1 = y1_;
+    this.x2 = x2_;
+    this.y2 = y2_;
+    this.amtPoints = amtPoints_;
+  }
+
+  show() {
+    for (let i = 0; i <= this.amtPoints; i++) {
+      let x = lerp(this.x1, this.x2, i / this.amtPoints);
+      let y = lerp(this.y1, this.y2, i / this.amtPoints);
+      vertex(x, y);
+    }
+  }
 }
